bd8a28757304b0c7f92970fdb9f4f0648a182f33
Amiqa Saleem <amiqasaleem@gmail.com>
2025-03-21 12:09:36 +0500

    changes made

--- app.mjs
+++ app.mjs
@@ -1,1 +1,11 @@
-console.log("Hello Worlddddd");
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get('/',(req, res)=>{
+    res.send('First practice of MongoDB')
+})
+app.listen(3000, ()=>{
+    console.log(`Amiqa listening on port ${port}`);
+    
+})
